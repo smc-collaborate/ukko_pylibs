b877424bc5bdade9e8bb73072b02cfe6c67abe2e
Mac Harwood <MacHarwood@gmail.com>
2026-07-28 15:01:39 +1000

    feat: Tweak default export list

--- __init__.py
+++ __init__.py
@@ -8,31 +8,38 @@ shared_dir = os.path.abspath(f"{os.path.dirname(__file__)}/../")
 if shared_dir not in sys.path:
     sys.path.append(shared_dir)
 
+
+###########################
+#
 from ukko_pylibs.basic.logger import appLog
 from ukko_pylibs.basic.simpleUtils import PrettyText
 from ukko_pylibs.basic.simpleUtils import EscapeMgr
 from ukko_pylibs.basic.simpleUtils import DictUtils
 from ukko_pylibs.basic.simpleUtils import Utils
 from ukko_pylibs.basic.simpleUtils import ImageInfo
-from ukko_pylibs.appAssist.appSupport import AppChoices
 
-from ukko_pylibs.appAssist.appSupport import Define as AppDefinition
 from ukko_pylibs.basic.prettyTable import PrettyTable
 
 import ukko_pylibs.basic.fileUtils as FileUtils
 from ukko_pylibs.basic import styling
 from ukko_pylibs.basic.class_HandledException import HandledException
-from ukko_pylibs.basic.simpleUtils import timestampObj_from_ns
-
+from ukko_pylibs.basic.simpleUtils import timestampObj_from_ns, time_ns_toText
 from ukko_pylibs.basic.simpleUtils import Utils
 
+###########################
+#
+from ukko_pylibs.appAssist.appSupport import AppChoices
+from ukko_pylibs.appAssist.appSupport import Define as AppDefinition
+from ukko_pylibs.appAssist.appSupport import appConfig
+import ukko_pylibs.appAssist.appSupport as app
 
+
+###########################
+#
 class DataTypes:
     from ukko_pylibs.basic.class_DataContents import DataContents as DataContents
     from ukko_pylibs.basic.class_JsonData import JsonDict as JsonDict
 
-
-import ukko_pylibs.appAssist.appSupport as app
 
 asJsonStr = Utils.asJsonStr
 
@@ -41,6 +48,7 @@ asJsonRStr = Utils.asJsonRStr
 __all__ = [
     "appLog",
     "AppDefinition",
+    "appConfig",
     "PrettyText",
     "EscapeMgr",
     "DictUtils",
@@ -52,6 +60,7 @@ __all__ = [
     "HandledException",
     "ImageInfo",
     "timestampObj_from_ns",
+    "time_ns_toText",
     "AppChoices",
     "asJsonStr",
     "asJsonRStr",
