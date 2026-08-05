8db947cf1099ad6b9858aa187a52542598a159bb
Mac Harwood <MacHarwood@gmail.com>
2026-08-05 10:39:52 +1000

    feat[appLogging]: Add JsonLinesLogger

--- pkgs/appLogging/__init__.py
+++ pkgs/appLogging/__init__.py
@@ -3,5 +3,12 @@
 
 from .app_logger import appLog, timeFromStart_text, timeFromStart_ms
 from .class_SimpleLogger import SimpleLogger
+from .class_JsonLinesLogger import JsonLinesLogger
 
-__all__ = ["appLog", "SimpleLogger", "timeFromStart_text", "timeFromStart_ms"]
+__all__ = [
+    "appLog",
+    "SimpleLogger",
+    "timeFromStart_text",
+    "timeFromStart_ms",
+    "JsonLinesLogger",
+]
