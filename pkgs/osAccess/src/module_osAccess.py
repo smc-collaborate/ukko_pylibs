7c7196aafdbef52f26b416c4b1ecec857c780e17
Mac Harwood <MacHarwood@gmail.com>
2026-08-13 18:24:12 +1000

    chore[ThreadedComms]: Omit 'null' entries from summary

--- pkgs/osAccess/src/module_osAccess.py
+++ pkgs/osAccess/src/module_osAccess.py
@@ -2,7 +2,7 @@ import subprocess
 import threading
 import time
 from typing import Any, Tuple
-from ukkoUtils import asJsonStr, ProgressMsg
+from ukkoUtils import asJsonStr, ProgressMsg, asUtf8orBytesOrNone
 
 
 class IAsyncAction_Interface:
@@ -67,6 +67,25 @@ class IAsyncAction_Interface:
         return False
 
 
+def bytesToReadable(src: bytes | None) -> list[str] | str | bytes | None:
+
+    result = asUtf8orBytesOrNone(src)
+    if isinstance(result, str):
+        lines = result.splitlines()
+        if len(lines) > 1:
+            result = lines
+    return result
+
+
+def setElementIfNonNull(obj: dict[str, Any], key: str, value: Any | None):
+    if value is not None:
+        obj[key] = value
+
+
+def setElement_asReadableBytes(obj: dict[str, Any], key: str, src: bytes | None):
+    setElementIfNonNull(obj, key, bytesToReadable(src))
+
+
 class ThreadedCommandRunner(IAsyncAction_Interface):
     """Runs as a daemon so will automatically be killed on exit"""
 
@@ -114,19 +133,19 @@ class ThreadedCommandRunner(IAsyncAction_Interface):
                 timeout=self.timeoutSeconds,
             )
             runResults["return_code"] = runOutputs.returncode
-            runResults["stdout"] = runOutputs.stdout
-            runResults["stderr"] = runOutputs.stdout
+            setElement_asReadableBytes(runResults, "stdout", runOutputs.stdout)
+            setElement_asReadableBytes(runResults, "stderr", runOutputs.stderr)
 
         except subprocess.CalledProcessError as ex:
             runResults["return_code"] = ex.returncode
-            runResults["stdout"] = ex.stdout
-            runResults["stderr"] = ex.stdout
+            setElement_asReadableBytes(runResults, "stdout", ex.stdout)
+            setElement_asReadableBytes(runResults, "stderr", ex.stderr)
             runResults["exception"] = f"CalledProcessError({ex})"
 
         except subprocess.TimeoutExpired as ex:
             runResults["exception"] = f"TimeoutExpired ({ex.timeout} seconds)"
-            runResults["stdout"] = ex.stdout
-            runResults["stderr"] = ex.stdout
+            setElement_asReadableBytes(runResults, "stdout", ex.stdout)
+            setElement_asReadableBytes(runResults, "stderr", ex.stderr)
         except FileNotFoundError:
             runResults["exception"] = "CommandNotFound" + (
                 "" if len(runThis) == 0 else f"[{runThis[0]}]"
